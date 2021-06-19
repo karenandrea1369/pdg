e60116c92841e6904c9045498f90bf8c77f12b53
karenandrea1369 <karenandrea12204@gmail.com>
2021-06-19 15:05:58 -0500

    Report section started

--- src/scripts/teacherCourses/course1.js
+++ src/scripts/teacherCourses/course1.js
@@ -37,6 +37,10 @@ window.addEventListener('load',()=>{
           });
     })
 
+    var studentsQuestionsCTA = document.getElementById("studentsQuestions").querySelector(".primaryBtn");
+    
+    var seeMoreAnswers = document.querySelectorAll(".coursecard__answersBtn");
+
     //----------------------teacher grades unit---------------
     var unitGrade = "";
     var unitComment = "";
@@ -66,6 +70,7 @@ window.addEventListener('load',()=>{
                 teacherGradeSeeMore.classList.add("coursecard__bottomBtn--visible");
                 unitGradeDiv.innerText = doc.data().teacherCalification;
                 unitCommentDiv.innerText = doc.data().teacherComment;
+                studentsQuestionsCTA.classList.remove("primaryBtn--disabled");
             }else {
                 // doc.data() will be undefined in this case
                 //console.log("No such document!");
@@ -88,7 +93,6 @@ window.addEventListener('load',()=>{
     var teacherGradeComment = document.getElementById("teacherGrade").querySelector(".coursecard__comment");
     teacherGradeComment.addEventListener('keyup', ()=>{
         unitComment = document.getElementById("teacherGrade").querySelector(".coursecard__comment").value;
-        console.log("comment", unitComment);
         validateGradeInput();
     })
 
@@ -104,7 +108,9 @@ window.addEventListener('load',()=>{
             }
         }
     }
+
     teacherGradeCTA.addEventListener('click', ()=>{
+        
         if(!teacherGradeSections[1].classList.contains("coursecard__section--visible")){
             teacherGradeSections[1].classList.add("coursecard__section--visible");
             validateGradeInput();
@@ -133,7 +139,9 @@ window.addEventListener('load',()=>{
             teacherGradeSeeMore.getElementsByTagName("p")[0].innerText = "Ver menos";
             unitGradeDiv.innerText = unitGrade;
             unitCommentDiv.innerText = unitComment;
+            studentsQuestionsCTA.classList.remove("primaryBtn--disabled");
         }
+        
     })
     
     teacherGradeCancel.addEventListener('click', ()=>{
@@ -189,7 +197,155 @@ window.addEventListener('load',()=>{
         })
     });
 
-    var aspects = ["","",""];
+    var studentsQuestions = document.querySelector(".coursecard__studentQuestion");
+
+    var studentsQuestionsStates = document.getElementById("studentsQuestions").querySelectorAll(".coursecard__bottomStateCard");
+    
+    var studentsQuestionsCancel = document.getElementById("studentsQuestions").querySelector(".secondaryBtn");
+    
+    getEvaluationSent();
+    function getEvaluationSent (){
+        db.collection("courses").doc("course1").collection("units").doc("unit3").get().then((doc) =>{
+            //.log(doc);
+            if(doc.exists && doc.data().evaluationSent){
+                console.log("Evaluación enviada", doc.data().evaluationSent);
+                studentsQuestions.classList.remove("coursecard__studentQuestion--visible");
+                studentsQuestionsStates[0].classList.remove("coursecard__bottomStateCard--visible");
+                studentsQuestionsStates[1].classList.add("coursecard__bottomStateCard--visible");
+                studentsQuestionsCTA.classList.remove("coursecard__bottomBtn--visible");
+                seeMoreAnswers[2].classList.remove("seeMoreBtn--disabled");
+            }else {
+                // doc.data() will be undefined in this case
+                //console.log("No such document!");
+            } 
+        });
+    }
+
+    validateUnitActivities();
+    function validateUnitActivities(){
+        if(studentsQuestions.classList.contains("coursecard__studentQuestion--visible")){
+            var emptyActivities = activities.filter(activity => activity == "");
+            //console.log(emptyActivities);
+            if(visibilityCheckboxes[3].checked){
+                if(emptyActivities.length <=2){
+                    //console.log("actividades activas");
+                    studentsQuestionsCTA.classList.remove("primaryBtn--disabled");
+                } else {
+                    studentsQuestionsCTA.classList.add("primaryBtn--disabled");
+                }
+            } else {
+                console.log("actividades inactivas");
+                studentsQuestionsCTA.classList.remove("primaryBtn--disabled");
+            }
+        }
+    }
+
+    studentsQuestionsCTA.addEventListener('click', ()=>{
+        if(!studentsQuestionsCTA.classList.contains("primaryBtn--disabled")){
+            if(!studentsQuestions.classList.contains("coursecard__studentQuestion--visible")){
+                studentsQuestions.classList.add("coursecard__studentQuestion--visible");
+                validateUnitActivities();
+                //aquí poner modo edicioooooooooon
+                document.getElementById("studentsQuestions").querySelector(".coursecard__bottomState").classList.remove("coursecard__bottomState--visible");
+                document.getElementById("studentsQuestions").querySelector(".coursecard__bottomStateCard").classList.remove("coursecard__bottomStateCard--visible");
+                studentsQuestionsCancel.classList.add("coursecard__bottomBtn--visible");
+            } else if(studentsQuestionsCTA.classList.contains("primaryBtn--disabled")){
+                console.log("nel");
+                document.getElementById("studentsQuestions").querySelector(".coursecard__bottomStateAlert").classList.add("coursecard__bottomStateAlert--visible");
+                //aquí poner mensaje de alertaaaaaa
+            } else {
+                setAnswersVisibility();
+                //poner aquí el estado ya calificado
+                studentsQuestionsCTA.classList.remove("primaryBtn--disabled");
+                studentsQuestionsCancel.classList.remove("coursecard__bottomBtn--visible");
+                document.getElementById("studentsQuestions").querySelector(".coursecard__bottomStateAlert").classList.remove("coursecard__bottomStateAlert--visible");
+                document.getElementById("studentsQuestions").querySelector(".coursecard__bottomState").classList.add("coursecard__bottomState--visible");
+                studentsQuestions.remove("coursecard__studentQuestion--visible");
+                studentsQuestionsStates[0].classList.remove("coursecard__bottomStateCard--visible");
+                studentsQuestionsStates[1].classList.add("coursecard__bottomStateCard--visible");
+                studentsQuestionsCTA.classList.remove("coursecard__bottomBtn--visible");
+                seeMoreAnswers[2].classList.remove("seeMoreBtn--disabled");
+            }
+        }
+    })
+    
+    studentsQuestionsCancel.addEventListener('click', ()=>{
+        studentsQuestionsCTA.classList.remove("primaryBtn--disabled");
+        document.getElementById("studentsQuestions").querySelector(".coursecard__bottomStateAlert").classList.remove("coursecard__bottomStateAlert--visible");
+        document.getElementById("studentsQuestions").querySelector(".coursecard__bottomState").classList.add("coursecard__bottomState--visible");
+        studentsQuestions.classList.remove("coursecard__studentQuestion--visible");
+        studentsQuestionsStates[0].classList.add("coursecard__bottomStateCard--visible");
+        studentsQuestionsCancel.classList.remove("coursecard__bottomBtn--visible");
+    });
+
+    function setAnswersVisibility(){
+        db.collection("courses").doc("course1").collection("units").doc("unit3").set({
+            evaluationSent : true,
+            question2 : visibilityCheckboxes[1].checked,
+            question3 : visibilityCheckboxes[2].checked,
+            question4 : visibilityCheckboxes[3].checked,
+            question6 : visibilityCheckboxes[4].checked
+        }, {merge:true}).then(() => {
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+    }
+
+    //----------------------teacher sees answers---------------
+    var answersSection = document.querySelectorAll(".coursecard__answers");
+    var answersBoxes = document.querySelectorAll(".answerBox");
+    var answersBoxesVisibility = [];
+
+    getAnswersVisibility();
+    function getAnswersVisibility (){
+        db.collection("courses").doc("course1").collection("units").doc("unit3").get().then((doc) =>{
+            //.log(doc);
+            if(doc.exists && doc.data().evaluationSent){
+                answersBoxesVisibility[0] = doc.data().question2;
+                answersBoxesVisibility[1] = doc.data().question3;
+                answersBoxesVisibility[2] = doc.data().question4;
+                answersBoxesVisibility[3] = doc.data().question6;
+                setAnswersVisibility();
+            }else {
+                // doc.data() will be undefined in this case
+                //console.log("No such document!");
+            } 
+        });
+    }
+
+    function setAnswersVisibility(){
+        if(answersBoxesVisibility[0]) answersBoxes[1].classList.add("answerBox--visible");
+        if(answersBoxesVisibility[1]) answersBoxes[2].classList.add("answerBox--visible");
+        if(answersBoxesVisibility[2]) answersBoxes[3].classList.add("answerBox--visible");
+        if(answersBoxesVisibility[3]) answersBoxes[4].classList.add("answerBox--visible"); 
+    }
+
+    seeMoreAnswers.forEach((btn, index) =>{
+        btn.addEventListener('click', ()=>{
+            if(!btn.classList.contains("seeMoreBtn--disabled")){
+                if(!btn.classList.contains("seeMoreBtn--opened")){
+                    btn.classList.add("seeMoreBtn--opened");
+                    btn.getElementsByTagName("p")[0].innerText = "Ocultar respuestas";
+                    answersSection[index].classList.add("coursecard__answers--visible");
+                } else {
+                    btn.classList.remove("seeMoreBtn--opened");
+                    btn.getElementsByTagName("p")[0].innerText = "Ver respuestas";
+                    answersSection[index].classList.remove("coursecard__answers--visible");
+                }
+            }
+        })
+    })
+
+});
+
+
+///-------------- code that I can't delete, just in case -------------
+
+/*
+
+var aspects = ["","",""];
     var activities = ["","","",""];
 
     var aspectsInputs = document.querySelectorAll(".selfEvaluationAspect");
@@ -209,102 +365,7 @@ window.addEventListener('load',()=>{
         })
     });
 
-    var studentsQuestions = document.querySelector(".coursecard__studentQuestion");
-
-    var studentsQuestionsStates = document.getElementById("studentsQuestions").querySelectorAll(".coursecard__bottomStateCard");
-
-    var studentsQuestionsCTA = document.getElementById("studentsQuestions").querySelector(".primaryBtn");
-    
-    var studentsQuestionsCancel = document.getElementById("studentsQuestions").querySelector(".secondaryBtn");
-    
-    getEvaluationSent();
-    function getEvaluationSent (){
-        console.log("entroooooooo");
-        db.collection("courses").doc("course1").collection("units").doc("unit3").get().then((doc) =>{
-            //.log(doc);
-            if(doc.exists && doc.data().evaluationSent){
-                console.log("Evaluación enviada", doc.data().evaluationSent);
-                studentsQuestions.classList.remove("coursecard__studentQuestion--visible");
-                studentsQuestionsStates[0].classList.remove("coursecard__bottomStateCard--visible");
-                studentsQuestionsStates[1].classList.add("coursecard__bottomStateCard--visible");
-                studentsQuestionsCTA.classList.remove("coursecard__bottomBtn--visible");
-            }else {
-                // doc.data() will be undefined in this case
-                //console.log("No such document!");
-            } 
-        });
-    }
-
-    validateUnitActivities();
-    function validateUnitActivities(){
-        if(studentsQuestions.classList.contains("coursecard__studentQuestion--visible")){
-            var emptyActivities = activities.filter(activity => activity == "");
-            //console.log(emptyActivities);
-            if(visibilityCheckboxes[3].checked){
-                if(emptyActivities.length <=2){
-                    console.log("actividades activas");
-                    studentsQuestionsCTA.classList.remove("primaryBtn--disabled");
-                } else {
-                    studentsQuestionsCTA.classList.add("primaryBtn--disabled");
-                }
-            } else {
-                console.log("actividades inactivas");
-                studentsQuestionsCTA.classList.remove("primaryBtn--disabled");
-            }
-        }
-    }
-
-    studentsQuestionsCTA.addEventListener('click', ()=>{
-        if(!studentsQuestions.classList.contains("coursecard__studentQuestion--visible")){
-            studentsQuestions.classList.add("coursecard__studentQuestion--visible");
-            validateUnitActivities();
-            //aquí poner modo edicioooooooooon
-            document.getElementById("studentsQuestions").querySelector(".coursecard__bottomState").classList.remove("coursecard__bottomState--visible");
-            document.getElementById("studentsQuestions").querySelector(".coursecard__bottomStateCard").classList.remove("coursecard__bottomStateCard--visible");
-            studentsQuestionsCancel.classList.add("coursecard__bottomBtn--visible");
-        } else if(studentsQuestionsCTA.classList.contains("primaryBtn--disabled")){
-            console.log("nel");
-            document.getElementById("studentsQuestions").querySelector(".coursecard__bottomStateAlert").classList.add("coursecard__bottomStateAlert--visible");
-            //aquí poner mensaje de alertaaaaaa
-        } else {
-            console.log("enviooooooooo");
-            setActivitiesAspects();
-            //poner aquí el estado ya calificado
-            studentsQuestionsCTA.classList.remove("primaryBtn--disabled");
-            studentsQuestionsCancel.classList.remove("coursecard__bottomBtn--visible");
-            document.getElementById("studentsQuestions").querySelector(".coursecard__bottomStateAlert").classList.remove("coursecard__bottomStateAlert--visible");
-            document.getElementById("studentsQuestions").querySelector(".coursecard__bottomState").classList.add("coursecard__bottomState--visible");
-            studentsQuestions.remove("coursecard__studentQuestion--visible");
-            studentsQuestionsStates[0].classList.remove("coursecard__bottomStateCard--visible");
-            studentsQuestionsStates[1].classList.add("coursecard__bottomStateCard--visible");
-            studentsQuestionsCTA.classList.remove("coursecard__bottomBtn--visible");
-        }
-    })
-    
-    studentsQuestionsCancel.addEventListener('click', ()=>{
-        studentsQuestionsCTA.classList.remove("primaryBtn--disabled");
-        document.getElementById("studentsQuestions").querySelector(".coursecard__bottomStateAlert").classList.remove("coursecard__bottomStateAlert--visible");
-        document.getElementById("studentsQuestions").querySelector(".coursecard__bottomState").classList.add("coursecard__bottomState--visible");
-        studentsQuestions.classList.remove("coursecard__studentQuestion--visible");
-        studentsQuestionsStates[0].classList.add("coursecard__bottomStateCard--visible");
-        studentsQuestionsCancel.classList.remove("coursecard__bottomBtn--visible");
-    });
-
-    function setActivitiesAspects(){
-        db.collection("courses").doc("course1").collection("units").doc("unit3").set({
-            evaluationSent : true,
-            question2 : visibilityCheckboxes[1].checked,
-            question3 : visibilityCheckboxes[2].checked,
-            question4 : visibilityCheckboxes[3].checked,
-            question5 : visibilityCheckboxes[4].checked
-        }, {merge:true}).then(() => {
-            console.log("Document successfully written!");
-        })
-        .catch((error) => {
-            console.error("Error writing document: ", error);
-        });
-
-        if(visibilityCheckboxes[3].checked){
+    if(visibilityCheckboxes[3].checked){
             db.collection("courses").doc("course1").collection("units").doc("unit3").set({
                 activity1 : activities[0],
                 activity2 : activities[1],
@@ -330,26 +391,5 @@ window.addEventListener('load',()=>{
                 console.error("Error writing document: ", error);
             });
         }
-    }
 
-
-    var answersSections = document.querySelectorAll(".coursecard__answers");
-    var seeMoreAnswers = document.querySelectorAll(".coursecard__answersBtn");
-
-    seeMoreAnswers.forEach((btn, index) =>{
-        btn.addEventListener('click', ()=>{
-            if(!btn.classList.contains("seeMoreBtn--disabled")){
-                if(!btn.classList.contains("seeMoreBtn--opened")){
-                    btn.classList.add("seeMoreBtn--opened");
-                    btn.getElementsByTagName("p")[0].innerText = "Ocultar respuestas";
-                    answersSections[index].classList.add("coursecard__answers--visible");
-                } else {
-                    btn.classList.remove("seeMoreBtn--opened");
-                    btn.getElementsByTagName("p")[0].innerText = "Ver respuestas";
-                    answersSections[index].classList.remove("coursecard__answers--visible");
-                }
-            }
-        })
-    })
-
-});
+*/
